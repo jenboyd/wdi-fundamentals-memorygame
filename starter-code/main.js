791b4bc623171c4a1f0e267964562040bc8c8aed
Jennifer Boyd <jboyd6290@gmail.com>
2016-08-18 18:45:46 -0400

    Added Javascript to change the DOM

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,19 +1,33 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardFour = "king";
 
-if (cardOne === cardFour) {
-	alert ("Sorry try again")
+// if (cardOne === cardFour) {
+// 	alert ("Sorry try again")
+// }
+// else if (cardOne === cardTwo){
+// 	alert ("You found a match!")
+// }
+// else if (cardOne === cardThree){
+// 	alert ("Sorry try again")
+// }
+// else if (cardThree === cardFour) {
+// 	alert ("You found a match!")
+// }
+
+createBoard = function () {
+	for (var i = 1; i < 5; i += 1) {
+		var board = document.getElementById("game-board");
+
+		var newDiv = document.createElement('div');
+
+		newDiv.className = "card";
+
+		document.getElementById("game-board").appendChild(newDiv);
+	}
 }
-else if (cardOne === cardTwo){
-	alert ("You found a match!")
-}
-else if (cardOne === cardThree){
-	alert ("Sorry try again")
-}
-else if (cardThree === cardFour) {
-	alert ("You found a match!")
-}
+
+createBoard()
